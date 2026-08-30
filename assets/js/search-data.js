464ba07e0c37b29ebf219bda624e11054e20044d
tinazhangzh <148819581+tinazhangzh@users.noreply.github.com>
2026-08-30 03:14:48 +0000

    Deploying to gh-pages from @ tinazhangzh/tinazhangzh.github.io@e4ef46b6af4220aace79de60255832ef27e92864 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,12 +44,7 @@ ninja.data = [{
           section: "News",},{id: "news-excited-to-begin-a-new-chapter-as-a-ph-d-student-in-statistics-at-the-ohio-state-university",
           title: '🎬 Excited to begin a new chapter as a Ph.D. student in Statistics...',
           description: "",
-          section: "News",},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
+          section: "News",},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
