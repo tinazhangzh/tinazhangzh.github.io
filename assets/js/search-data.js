577b8d92672dfd699f7a1db8b0ceeefb80dfa5bd
tinazhangzh <148819581+tinazhangzh@users.noreply.github.com>
2026-08-30 03:13:50 +0000

    Deploying to gh-pages from @ tinazhangzh/tinazhangzh.github.io@918c7fb89d4bac9c4cb8008217c0b74f4a1dd8e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,17 +44,7 @@ ninja.data = [{
           section: "News",},{id: "news-excited-to-begin-a-new-chapter-as-a-ph-d-student-in-statistics-at-the-ohio-state-university",
           title: '🎬 Excited to begin a new chapter as a Ph.D. student in Statistics...',
           description: "",
-          section: "News",},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
+          section: "News",},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
