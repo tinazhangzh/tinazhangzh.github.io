f5b221b8235a4727816ca8d06e462d044cbd5d25
tinazhangzh <148819581+tinazhangzh@users.noreply.github.com>
2026-08-30 04:07:55 +0000

    Deploying to gh-pages from @ tinazhangzh/tinazhangzh.github.io@182859a59057230cc1cbd83bde923769dace2f82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-service",
+          title: "Service",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/service/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
